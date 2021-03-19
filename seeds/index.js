63aaf63df58efcad3592a2c62d56cbdf413c5422
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-03-19 13:26:40 -0400

    updated seeds

---
+++ seeds/index.js
@@ -0,0 +1,22 @@
+const sequelize = require('../config/connection');
+const seedComment = require('./commentData.json');
+const seedPost = require('./postData.json');
+const seedUser = require('./userData.json')
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED ----\n');
+
+    await seedUser();
+    console.log('\n-----USER SEEDED----\n');
+
+    await seedPost();
+    console.log('\n-----POST SEEDED----\n');
+
+    await seedComment();
+    console.log('\n-----COMMENT SEEDED----\n');
+
+    process.exit(0);
+};
+
+seedAll();
