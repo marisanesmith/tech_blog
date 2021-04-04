0abb0beb6542dac6a865dc734d4dddc7625935a4
marisanesmith <marisa@thisworldtraveled.com>
2021-04-04 15:30:04 -0400

    made updates to the seed files

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -3,15 +3,18 @@ const { Blog } = require('../models');
 const blogData = [
     {
         "title": "Learning all about handlebars and MVC",
-        "content": "Each day with proper studying I am learning more about coding. This includes full stack web development where you include the front end and the back end."
+        "content": "Each day with proper studying I am learning more about coding. This includes full stack web development where you include the front end and the back end.",
+        "user_id": 1
     },
     {
         "title": "Learning all about handlebars and MVC",
-        "content": "Each day with proper studying I am learning more about coding. This includes full stack web development where you include the front end and the back end."
+        "content": "Each day with proper studying I am learning more about coding. This includes full stack web development where you include the front end and the back end.",
+        "user_id": 1
     },
     {
         "title": "Learning all about handlebars and MVC",
-        "content": "Each day with proper studying I am learning more about coding. This includes full stack web development where you include the front end and the back end."
+        "content": "Each day with proper studying I am learning more about coding. This includes full stack web development where you include the front end and the back end.",
+        "user_id": 1
     }
 ];
 
