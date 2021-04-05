5156529a8c12a0ffb9e6d59c413f8a315be089e3
marisanesmith <marisa@thisworldtraveled.com>
2021-04-05 17:19:11 -0400

    updates to posts and main handlebar page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'QuIeT',
+  secret: 'pAsSwOrD',
   cookie: {},
   resave: false,
   saveUninitialized: true,
